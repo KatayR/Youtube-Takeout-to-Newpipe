4a5bff3dafef97f49777bdb557ef5a5b21875933
KatayR <rasitarslan@outlook.com>
2023-04-20 01:04:44 +0300

    first test with "youtube-dl"

--- youtube_metadata_extractor.py
+++ youtube_metadata_extractor.py
@@ -8,27 +8,33 @@ import googleapiclient.discovery
 import googleapiclient.errors
 import re
 
-# ready Youtube API
-api_key = os.environ['YOUTUBE_API_KEY']
-youtube = googleapiclient.discovery.build(
-    'youtube', 'v3', developerKey=api_key)
-
-# deal with youtubes time format like PT23M21S
+import youtube_dl
 
 
-def yt_time_to_seconds(time):
-    regex_string = 'PT((?P<minutes>\d+)M)?((?P<seconds>\d+)S)?'
-    regex = re.compile(regex_string)
-    match = regex.match(time)
-    if match:
-        minutes = int(match.group('minutes')) if match.group('minutes') else 0
-        seconds = int(match.group('seconds')) if match.group('seconds') else 0
-        return minutes * 60 + seconds
-    return 0
+ydl_opts = {}
+
+
+# # ready Youtube API
+# api_key = os.environ['YOUTUBE_API_KEY']
+# youtube = googleapiclient.discovery.build(
+#     'youtube', 'v3', developerKey=api_key)
+
+# # deal with youtubes time format like PT23M21S
+
+
+# def yt_time_to_seconds(time):
+#     regex_string = 'PT((?P<minutes>\d+)M)?((?P<seconds>\d+)S)?'
+#     regex = re.compile(regex_string)
+#     match = regex.match(time)
+#     if match:
+#         minutes = int(match.group('minutes')) if match.group('minutes') else 0
+#         seconds = int(match.group('seconds')) if match.group('seconds') else 0
+#         return minutes * 60 + seconds
+#     return 0
 
 
 # Define the URL of the HTML file containing the data
-with open("C:/Users/rasit/Desktop/watch_history.html", 'r', encoding='utf-8') as url:
+with open("C:/Users/rasit/Desktop/m_watch_history.html", 'r', encoding='utf-8') as url:
     soup = BeautifulSoup(url, "html.parser")
 
 # Find all the video links in the HTML
@@ -68,35 +74,33 @@ for videos in blocks:
 
     # Call Youtube api for more info. Idk if this makes it faster or
     # slower relative to using youtube-dl, BeautifulSoup and stuff
-    request = youtube.videos().list(
-        part="snippet,contentDetails,statistics",
-        id=video_id
-    )
-    response = request.execute()
-    print(title + "\n" + url)
+    # request = youtube.videos().list(
+    #     part="snippet,contentDetails,statistics",
+    #     id=video_id
+    # )
+    # response = request.execute()
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        scraped = ydl.extract_info(url, download=False)
 
     try:  # this part checks if video is hidden. Yes, I know. But youtube API is broken. So I had to...
-        duration_response = response['items'][0]['contentDetails']['duration']
-        uploadDate_response = response['items'][0]['snippet']['publishedAt'][:10]
+        vid_duration = scraped['duration']
+        uploadDate_response = scraped['upload_date']
     except:
-        duration_response = 'PT4M04S'
-        uploadDate_response = '2000-04-04'
+        vid_duration = 'PT4M04S'
+        uploadDate_response = '20000404'
 
     # This checks if video is unlisted AND "you"(your api) can't access it. Api is weird.
     try:
-        viewcount_response = response['items'][0]['statistics']['viewCount']
+        view_count = scraped['view_count']
     except:
-        viewcount_response = 404
+        view_count = 404
 
-    textual_upload_date = uploadDate_response
+    textual_upload_date = uploadDate_response[:4] + '-' + \
+        uploadDate_response[4:6] + '-' + uploadDate_response[6:]
     struct_time = time.strptime(
         textual_upload_date, "%Y-%m-%d")
     seconds_since_epoch = time.mktime(struct_time)
     upload_date = int(seconds_since_epoch * 1000)
-
-    vid_duration = yt_time_to_seconds(duration_response)
-
-    view_count = viewcount_response
 
     # Construct the thumbnail URL
     thumbnail_url = f"https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg"
@@ -107,8 +111,10 @@ for videos in blocks:
 
     # Increment the uid value
     print(f"{uid-1900} done\n")
+    if uid == 1902:
+        break
     uid += 1
 # Close the CSV file
 end = time.time()
-print("it took" + (end - start) + "seconds")
+print('it took ' + '{:.2f}'.format(end - start) + ' seconds')
 csv_file.close()
